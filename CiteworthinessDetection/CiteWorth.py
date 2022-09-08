4b8f5102a6f3a8c9d88570517dcd02642821a869
csacro <50425802+csacro@users.noreply.github.com>
2022-09-08 21:52:12 +0200

    correct licensing of CiteWorth code (MIT license)

--- CiteworthinessDetection/CiteWorth.py
+++ CiteworthinessDetection/CiteWorth.py
@@ -1,6 +1,6 @@
 """
-    The Implementation below is an adapted (but still copied) version from https://github.com/copenlu/cite-worth,
-        which is the original implementation by Dustin Wright and Isabelle Augenstein from the Paper
+    The Implementation below is a modified version of https://github.com/copenlu/cite-worth,
+    which is the original CiteWorth implementation by Dustin Wright and Isabelle Augenstein from the Paper
         @inproceedings{wright2021citeworth,
                 title={{CiteWorth: Cite-Worthiness Detection for Improved Scientific Document Understanding}},
                 author={Dustin Wright and Isabelle Augenstein},
@@ -8,6 +8,29 @@
                 publisher = {Association for Computational Linguistics},
                 year = 2021
             }
+    The implementation in this file is licensed under the MIT License.
+
+    MIT License
+
+    Copyright (c) 2021 CopeNLU and Carolin Schindler, respectively (see https://github.com/Data-Science-2Like/cite-worth-with-section-info)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 """
 
 from typing import List, Tuple, AnyStr
